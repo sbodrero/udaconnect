6fbd4fcb54238d75a6244b3be93fee562cbeb9e4
sbodrero <bodrero.sebastien@gmail.com>
2021-09-13 16:24:42 +0200

    updated eb config

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -24,7 +24,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(cors({origin: '*'}));
 
-  app.use("/api/v0/", IndexRouter);
+  app.use("/api/v0/", cors(), IndexRouter);
 
   // Root URI call
   app.get("/", async (req, res) => {
